0f89cc5518b7312822e52d0a37f8405d05354b97
AlynneAyumi <alynneak@outlook.com>
2024-10-16 21:34:01 -0300

    Menu Lateral -Ayumi

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sideBar").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sideBar").style.width   
+   = "0";
+  }
