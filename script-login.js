909812443ff25b613b2cb87deedc02c7c4e82612
Wendryel <wendryelbruneto@gmail.com>
2024-10-30 22:48:52 -0300

    Tela de login -Wendryel

---
+++ script-login.js
@@ -0,0 +1,20 @@
+localStorage.setItem('usuario', 'admin');
+localStorage.setItem('senha', 'senha123');
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const usuarioLogin = document.getElementById('usuario').value;
+    const senhaLogin = document.getElementById('senha').value;
+
+    const usuarioSalvo = localStorage.getItem('usuario');
+    const senhaSalva = localStorage.getItem('senha');
+
+
+    if (usuarioLogin === usuarioSalvo && senhaLogin === senhaSalva) {
+        window.location.href = 'index.html';
+
+    } else {
+        alert('Email ou senha incorretos.');
+    }
+});
