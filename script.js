47b8e6d0b81e5cbbf50067bd48464d183cbd7337
Wendryel <wendryelbruneto@gmail.com>
2024-11-01 07:09:10 -0300

    Criação da tela principal -Wendryel

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function openNav() {
     document.getElementById("sideBar").style.width   
    = "0";
   }
+
+
+/* Tela Principal ----------------------- */
+
+function search() {
+  const table = document.getElementById("resultTable");
+
+  for (let i = 0; i < 10; i++) {
+      const row = table.insertRow();
+
+      const nome = row.insertCell(0);
+      const autos = row.insertCell(1);
+      const genero = row.insertCell(2);
+      const vara = row.insertCell(3);
+      const observacao = row.insertCell(4);
+
+      nome.textContent = "------------";
+      autos.textContent = "------------";
+      genero.textContent = "------------ ";
+      vara.textContent = "------------ ";
+      observacao.textContent = "------------ ";
+  }
+}
