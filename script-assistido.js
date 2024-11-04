53c834ee92cde3a578b74ef61fa527b59479a9b8
Yel <yeldras@gmail.com>
2024-11-04 19:44:03 -0300

    Tela Assistido -Yel

---
+++ script-assistido.js
@@ -0,0 +1,7 @@
+
+const botao = document.getElementById('btnTeste');
+const conteudo = document.getElementById('conteudo');
+
+botao.addEventListener('click', () => {
+    conteudo.style.display = 'none';
+});
