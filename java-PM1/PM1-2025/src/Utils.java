ecec78efaf39550e68673fc20bd1151dbebd5564
Wendryel <wendryelbruneto@gmail.com>
2025-03-10 21:28:30 -0300

    Adicionando relatório -Wendryel

--- java-PM1/PM1-2025/src/Utils.java
+++ java-PM1/PM1-2025/src/Utils.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 
 public class Utils {
     static void limpaTela() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+
+        try {
+            String sistemaOperacional = System.getProperty("os.name");
+
+            if (sistemaOperacional.contains("Windows")) {
+                // Limpa a tela no Windows
+                ProcessBuilder builder = new ProcessBuilder("cmd", "/c", "cls");
+                builder.inheritIO().start().waitFor();
+            } else {
+                // Limpa a tela no Linux/Mac
+                ProcessBuilder builder = new ProcessBuilder("clear");
+                builder.inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     static void delay(int segundos) {
