21ab30855865fd64bfc6c0b6aceeb1367c99b7c3
AlynneAyumi <alynneak@outlook.com>
2025-11-27 20:52:12 -0300

    Remoção dos arquivos de teste

--- src/main/java/com/example/away/repository/UsuarioRepository.java
+++ src/main/java/com/example/away/repository/UsuarioRepository.java
@@ -3,6 +3,10 @@ package com.example.away.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.away.model.Usuario;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     Usuario findByEmail(String email);
+
+    Optional<Usuario> findByNomeUser(String nomeUser);
 }
